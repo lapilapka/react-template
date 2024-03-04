a4d38e6b52d71ad56f35dcdee7451b86dc10b422
e.belousova <e.belousova@finflowsoft.com>
2024-03-04 18:38:33 +0300

    Initial commit

---
+++ src/module.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import maks from './img/pic.png';
+const Module = () => (
+    <>
+    <h1>Hello React</h1>
+        <img src={maks} />
+    </>
+);
+
+export default Module;
